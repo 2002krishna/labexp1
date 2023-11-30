10aa620518033d84a03b8a31669e302c5124c6ad
Krishna Vamsi <93367881+2002krishna@users.noreply.github.com>
2023-12-01 00:33:35 +0530

    Update weather_modelling.py
    version3

--- weather_modelling.py
+++ weather_modelling.py
@@ -1,9 +1,9 @@
 def calculate_temperature(a, b, c, day):
     return a * (day ** 2) + b * day + c
 
-a_coefficient = float(input("Enter coefficient 'a': "))
-b_coefficient = float(input("Enter coefficient 'b': "))
-c_coefficient = float(input("Enter coefficient 'c': "))
+with open('coefficients.csv', 'r') as file:
+    lines = file.readlines()
+    a_coefficient, b_coefficient, c_coefficient = map(float, lines)
 input_day = int(input("Enter the day: "))
 
 result = calculate_temperature(a_coefficient, b_coefficient, c_coefficient, input_day)
