3a24879043ebb389ab7f10a7416c5d76728dea6e
Krishna Vamsi <93367881+2002krishna@users.noreply.github.com>
2023-12-01 00:32:42 +0530

    Update weather_modelling.py
    version_2

--- weather_modelling.py
+++ weather_modelling.py
@@ -1,10 +1,10 @@
 def calculate_temperature(a, b, c, day):
     return a * (day ** 2) + b * day + c
 
-a_coefficient = 2
-b_coefficient = 5
-c_coefficient = 3
-input_day = 10
+a_coefficient = float(input("Enter coefficient 'a': "))
+b_coefficient = float(input("Enter coefficient 'b': "))
+c_coefficient = float(input("Enter coefficient 'c': "))
+input_day = int(input("Enter the day: "))
 
 result = calculate_temperature(a_coefficient, b_coefficient, c_coefficient, input_day)
 print(f"Temperature on day {input_day} is {result} degrees.")
